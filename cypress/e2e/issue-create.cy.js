85e10ab780bd1008014f35915c767cdf7c42724c
joonaskr <57299327+joonaskr@users.noreply.github.com>
2024-10-22 10:44:05 +0300

    second up

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -8,6 +10,81 @@ describe('Issue create', () => {
         cy.visit(url + '/board?modal-issue-create=true');
       });
   });
+
+  it('Should create a Bug issue with highest priority and validate it on the board', () => {
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+      
+      cy.get('.ql-editor').type('My bug description');
+      cy.get('.ql-editor').should('have.text', 'My bug description');
+
+      cy.get('input[name="title"]').type('Bug');
+      cy.get('input[name="title"]').should('have.value', 'Bug');
+
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('[data-testid="select-option:Bug"]').trigger('mouseover').trigger('click');
+      cy.get('[data-testid="icon:bug"]').should('be.visible');
+
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+      cy.get('[data-testid="form-field:userIds"]').click();
+      cy.get('[data-testid="select-option:Lord Gaben"]').click();
+
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Highest"]').click();
+
+      cy.get('button[type="submit"]').click();
+    });
+
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains('Bug').should('be.visible').within(() => {
+        cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
+        cy.get('[data-testid="icon:bug"]').should('be.visible');
+      });
+    });
+  });
+
+  it('Should create a Task issue with medium priority and validate it on the board', () => {
+    
+    const title = faker.lorem.words(3);
+    const description = faker.lorem.sentence();
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+
+      cy.get('.ql-editor').type(description);
+      cy.get('.ql-editor').should('have.text', description);
+      cy.get('input[name="title"]').type(title);
+      cy.get('input[name="title"]').should('have.value', title);
+
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+      cy.get('[data-testid="form-field:userIds"]').click();
+      cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+      cy.get('button[type="submit"]').click();
+    });
+
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains('Task').should('be.visible').within(() => {
+        cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
+        cy.get('[data-testid="icon:task"]').should('be.visible');
+      });
+    });
+  });
+
+
 
   it('Should create an issue and validate it successfully', () => {
     // System finds modal for creating issue and does next steps inside of it
@@ -21,7 +98,7 @@ describe('Issue create', () => {
       // Otherwise filling title first sometimes doesn't work due to web page implementation
       cy.get('input[name="title"]').type('TEST_TITLE');
       cy.get('input[name="title"]').should('have.value', 'TEST_TITLE');
-      
+
 
       // Open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
@@ -87,4 +164,8 @@ describe('Issue create', () => {
       cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
     });
   });
+
+
+
+
 });
