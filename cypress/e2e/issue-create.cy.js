eb64701cb56061d6398a4aec09a560f193eafcec
joonaskr <57299327+joonaskr@users.noreply.github.com>
2024-10-22 11:20:17 +0300

    second up

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -11,44 +11,39 @@ describe('Issue create', () => {
       });
   });
 
-  it('Should create a Bug issue with highest priority and validate it on the board', () => {
+  it.only('Should create a Task issue using random data and validate it on the board', () => {
+    const title = faker.lorem.word();
+    const description = faker.lorem.words(5);
+  
     cy.get('[data-testid="modal:issue-create"]').within(() => {
-      
-      cy.get('.ql-editor').type('My bug description');
-      cy.get('.ql-editor').should('have.text', 'My bug description');
-
-      cy.get('input[name="title"]').type('Bug');
-      cy.get('input[name="title"]').should('have.value', 'Bug');
-
-      cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Bug"]').trigger('mouseover').trigger('click');
-      cy.get('[data-testid="icon:bug"]').should('be.visible');
+      cy.get('.ql-editor').type(description);
+      cy.get('.ql-editor').should('have.text', description);
+      cy.get('input[name="title"]').type(title);
+      cy.get('input[name="title"]').should('have.value', title);
 
       cy.get('[data-testid="select:reporterId"]').click();
-      cy.get('[data-testid="select-option:Pickle Rick"]').click();
-
-      cy.get('[data-testid="form-field:userIds"]').click();
-      cy.get('[data-testid="select-option:Lord Gaben"]').click();
-
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+  
       cy.get('[data-testid="select:priority"]').click();
-      cy.get('[data-testid="select-option:Highest"]').click();
-
+      cy.get('[data-testid="select-option:Low"]').click();
+  
       cy.get('button[type="submit"]').click();
     });
-
+  
     cy.get('[data-testid="modal:issue-create"]').should('not.exist');
     cy.contains('Issue has been successfully created.').should('be.visible');
-
+  
     cy.reload();
     cy.contains('Issue has been successfully created.').should('not.exist');
+  
 
     cy.get('[data-testid="board-list:backlog"]').within(() => {
-      cy.contains('Bug').should('be.visible').within(() => {
-        cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
-        cy.get('[data-testid="icon:bug"]').should('be.visible');
+      cy.contains(title).should('be.visible').within(() => {
+        cy.get('[data-testid="icon:task"]').should('be.visible');
       });
     });
   });
+  
 
   it('Should create a Task issue with medium priority and validate it on the board', () => {
     
